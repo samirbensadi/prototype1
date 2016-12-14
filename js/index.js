9a25a333f4a0c4ff7235f7213a41cd1bb10a0bba
Nabil Ghedjati <nabil.ghedjati@gmail.com>
2016-12-14 16:30:43 +0100

    ajout pages HA, inscription et parametres

--- js/index.js
+++ js/index.js
@@ -48,104 +48,99 @@ $(document).on("pageinit", "#home", function () {
     });
 
 
-    //ACHAT//
-
-
-    //PRIX
-    var prix1 = 2.50;
-    var prix2 = 3;
-    var prix3 = 3.50;
-
-    //GET LOCALSTOARAGE
-    var jaune = localStorage.getItem('jaune');
-    var vert = localStorage.getItem('vert');
-    var violet = localStorage.getItem('violet');
-    //TOTAL
-    var total1 = prix1 * jaune;
-    var total2 = prix2 * vert;
-    var total3 = prix3 * violet;
-    //PRIX FINAL
-    var totalfinal = total1 + total2 + total3;
-
-
-    //recupe prix pour afficher
-    function prix(){
-        document.getElementById('prix1').innerHTML = prix1 + '€';
-        document.getElementById('prix2').innerHTML = prix2 + '€';
-        document.getElementById('prix3').innerHTML = prix3 + '€';
-    }
-    //ticket jaune
-    var numbre1 = 0;
-
-    function add1() {
-        numbre1++;
-        document.getElementById('num1').innerHTML = numbre1;
-    }
-    function remove1() {
-        if (numbre1<1) {
-            numbre1 = 0;
-        }
-        else {
-            numbre1--;
-            document.getElementById('num1').innerHTML = numbre1;
-        }
-    }
-
-    //ticket vert
-    var numbre2 = 0;
-
-    function add2() {
-        numbre2++;
-        document.getElementById('num2').innerHTML = numbre2;
-    }
-    function remove2() {
-        if (numbre2<1) {
-            numbre2 = 0;
-        }
-        else {
-            numbre2--;
-            document.getElementById('num2').innerHTML = numbre2;
-        }
-    }
-
-    //ticket violet
-    var numbre3 = 0;
-
-    function add3() {
-        numbre3++;
-        document.getElementById('num3').innerHTML = numbre3;
-    }
-
-    function remove3() {
-        if (numbre3<1) {
-            numbre3 = 0;
-        }
-        else {
-            numbre3--;
-            document.getElementById('num3').innerHTML = numbre3;
-        }
-    }
-
-    //ENREGISTRE
-    function enregistre() {
-        localStorage.setItem('jaune', numbre1);
-        localStorage.setItem('vert', numbre2);
-        localStorage.setItem('violet', numbre3);
-    }
-    //PANIER
-    function panier() {
-
-        //affiche nombre de ticket
-        document.getElementById('jaune').innerHTML = jaune + ' ticket jaune a '+ prix1 +'€ dans votre panier';
-        document.getElementById('vert').innerHTML = vert + ' ticket vert a '+ prix2 +'€ dans votre panier';
-        document.getElementById('violet').innerHTML = violet + ' ticket violet a '+ prix3 +'€ dans votre panier';
-
-        //affiche prix
-        document.getElementById('total1').innerHTML = 'TOTAL: ' + total1 +'€';
-        document.getElementById('total2').innerHTML = 'TOTAL: ' + total2 +'€';
-        document.getElementById('total3').innerHTML = 'TOTAL: ' + total3 +'€';
-
-        document.getElementById('totalfinal').innerHTML = 'TOTAL: ' + totalfinal +'€';
-    }
-
 });
+
+// VANILLA JS
+
+//ACHAT//
+
+//NombreTicket
+var nombre1 = 0;
+var nombre2 = 0;
+var nombre3 = 0;
+
+//PRIX
+var prix1 = 2.50;
+var prix2 = 3;
+var prix3 = 3.50;
+
+//recupe prix pour afficher
+function prix(){
+    document.getElementById('prix1').innerHTML = prix1 + '€';
+    document.getElementById('prix2').innerHTML = prix2 + '€';
+    document.getElementById('prix3').innerHTML = prix3 + '€';
+}
+//ticket jaune
+function add1() {
+    nombre1++;
+    document.getElementById('num1').innerHTML = nombre1;
+}
+function remove1() {
+    if (nombre1<1) {
+        nombre1 = 0;
+    }
+    else {
+        nombre1--;
+        document.getElementById('num1').innerHTML = nombre1;
+    }
+}
+
+//ticket vert
+function add2() {
+    nombre2++;
+    document.getElementById('num2').innerHTML = nombre2;
+}
+function remove2() {
+    if (nombre2<1) {
+        nombre2 = 0;
+    }
+    else {
+        nombre2--;
+        document.getElementById('num2').innerHTML = nombre2;
+    }
+}
+
+//ticket violet
+function add3() {
+    nombre3++;
+    document.getElementById('num3').innerHTML = nombre3;
+}
+
+function remove3() {
+    if (nombre3<1) {
+        nombre3 = 0;
+    }
+    else {
+        nombre3--;
+        document.getElementById('num3').innerHTML = nombre3;
+    }
+}
+
+//PANIER
+function panier() {
+    //set localStorage
+    localStorage.setItem('ticket1', nombre1);
+    localStorage.setItem('ticket2', nombre2);
+    localStorage.setItem('ticket3', nombre3);
+
+    //Calcule PrixTotal
+    var total1 = nombre1 * prix1;
+    var total2 = nombre2 * prix2;
+    var total3 = nombre3 * prix3;
+    var prixTotal = total1 + total2 + total3;
+
+    localStorage.setItem('prixtotal', prixTotal);
+
+    //affiche nombre de ticket
+    document.getElementById('jaune').innerHTML = nombre1 + ' ticket jaune a '+ prix1 +'€ dans votre panier';
+    document.getElementById('vert').innerHTML = nombre2 + ' ticket vert a '+ prix2 +'€ dans votre panier';
+    document.getElementById('violet').innerHTML = nombre3 + ' ticket violet a '+ prix3 +'€ dans votre panier';
+
+    //affiche prix
+    document.getElementById('total1').innerHTML = 'TOTAL: ' + prix1 * nombre1 + '€';
+    document.getElementById('total2').innerHTML = 'TOTAL: ' + prix2 * nombre2 + '€';
+    document.getElementById('total3').innerHTML = 'TOTAL: ' + prix3 * nombre3 + '€';
+
+    document.getElementById('totalfinal').innerHTML = 'TOTAL: ' + prixTotal + '€';
+
+}
