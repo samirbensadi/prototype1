f9f5df33f193eb6a4bc5a70526412147a78b1e67
Nabil Ghedjati <nabil.ghedjati@gmail.com>
2016-12-21 13:23:07 +0100

    modif js inscription et php

--- js/inscription.js
+++ js/inscription.js
@@ -14,39 +14,50 @@ $(document).on("pageinit", "#inscription", function () {
     var mdpconf = $('#mdpconf').val();
 
     var regexNom = /[a-zA-Z]/;
+    var regexDate = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
     var regexTel = /[0-9]/;
     var regexMail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
 
+
     if (regexNom.test(nom) && nom.length > 2 && nom.length < 250) {
       if (regexNom.test(prenom) && prenom.length > 2 && prenom.length < 250) {
-        if (regexTel.test(tel) && tel.length >= 10 ) {
-          if (regexMail.test(email) && email == emailconf) {
-            if (mdpreg.length > 5 && mdpreg == mdpconf) {
-              $.ajax({
-                method: "POST",
-                url: 'http://192.168.1.46/prototype1/php/traitement_inscription.php',
-                data: $('#forminscription').serialize(),
-                success: function (data) {
-                  var reponse = JSON.parse(data);
-                  console.log(reponse);
-                  $.mobile.changePage($('#confInscription'),{transition : "slide", reverse: false});
-                },
-                error: function () {
-                  alert('Ya eu un problème !');
-                }
-              });
-            }
-            else {
-              alert("Vous n'avez pas entré de mot de passe !");
-            }
+        if (regexDate.test(bday)) {
+          if (regexTel.test(tel) && tel.length >= 10 ) {
+            if (regexMail.test(email) && email == emailconf) {
+              if (mdpreg.length > 5 && mdpreg == mdpconf) {
+                $.ajax({
+                  method: "POST",
+                  url: 'http://192.168.1.46/prototype1/php/traitement_inscription.php',
+                  data: $('#forminscription').serialize(),
+                  success: function (data) {
+                    var reponse = JSON.parse(data);
+                    console.log(reponse);
+                    if (reponse == "nothing received") {
+                      alert("Erreur : aucun champ n'a été renseigné !")
+                    }
+                    else if () {
 
+                    }
+                    $.mobile.changePage($('#confInscription'),{transition : "slide", reverse: false});
+                  },
+                  error: function () {
+                    alert('Ya eu un problème !');
+                  }
+                });
+              } else {
+                alert("Vous n'avez pas entré de mot de passe !");
+              }
+
+            } else {
+              alert("Votre e-mail n'est pas valide");
+            }
           } else {
-            alert("Votre e-mail n'est pas valide");
+            alert("Votre numéro n'est pas valide");
           }
+        } else {
+          alert("Votre date n'est pas valide");
         }
-        else {
-          alert("Votre numéro n'est pas valide");
-        }
+
       } else {
         alert("Votre prénom n'est pas valide");
       }
--- php/traitement_inscription.php
+++ php/traitement_inscription.php
@@ -2,24 +2,31 @@
 
 header("Access-Control-Allow-Origin: *"); // pour que tout le monde puisse interroger ce script
 
+$regexNom = "#^([a-zàáâäçèéêëìíîïñòóôöùúûüA-Z]+(( |')[a-zàáâäçèéêëìíîïñòóôöùúûüA-Z]+)*)+([-]([a-zàáâäçèéêëìíîïñòóôöùúûüA-Z]+(( |')[a-zàáâäçèéêëìíîïñòóôöùúûüA-Z]+)*)+)*$#iu";
 
-if (isset($_POST) && !empty($_POST)) {
-  $nom = $_POST['nom'];
-  $prenom = $_POST['prenom'];
-  $bday = $_POST['bday'];
-  $tel = $_POST['tel'];
-  $email = $_POST['email'];
-  $emailconf = $_POST['emailconf'];
-  $mdp = $_POST['mdp'];
-  $mdpconf = $_POST['mdpconf'];
-  $formation = $_POST['formation'];
+if (isset($_POST) && !empty($_POST)) { // si la superglobale existe et qu'elle n'est pas vide
+  // $nom = $_POST['nom'];
+  // $prenom = $_POST['prenom'];
+  // $bday = $_POST['bday'];
+  // $tel = $_POST['tel'];
+  // $email = $_POST['email'];
+  // $emailconf = $_POST['emailconf'];
+  // $mdp = $_POST['mdp'];
+  // $mdpconf = $_POST['mdpconf'];
+  // $formation = $_POST['formation'];
+  //
+  // $reponse = array('nom' =>  $nom, 'prenom' => $prenom, 'bday' => $bday, 'tel' => $tel, 'email' => $email, 'formation' => $formation );
 
-  $reponse = array('nom' =>  $nom, 'prenom' => $prenom, 'bday' => $bday, 'tel' => $tel, 'email' => $email, 'formation' => $formation );
+  if (isset($_POST['nom'], $_POST['prenom']) && !empty($_POST['nom'], $_POST['prenom']) && preg_match($regexNom, $_POST['nom']) && preg_match($regexNom, $_POST['prenom']) ) {
+  } else {
+    $reponse = "Votre nom et/ou prénom n'a pas été soumis )
+  }
+
 
 
 }
 else {
-  $reponse = false;
+  $reponse = "nothing received";
 }
 
 $reponsejs = json_encode($reponse);
